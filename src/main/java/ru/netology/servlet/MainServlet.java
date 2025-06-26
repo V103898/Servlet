a08a19dd048ad98a5c45d1141392ca4301a096fd
CTACOB <star2203@yandex.ru>
2025-06-26 20:36:45 +0300

    init di-java

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,7 +1,4 @@
-
 package ru.netology.servlet;
-import jakarta.servlet.annotation.ServletSecurity;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -10,11 +7,9 @@ import ru.netology.controller.PostController;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import java.util.regex.Pattern;
 
-
 public class MainServlet extends HttpServlet {
     private static final String POSTS_PATH = "/api/posts";
     private static final String POSTS_ID_PATH = "/api/posts/\\d+";
-
     private static final Pattern ID_PATTERN = Pattern.compile("/api/posts/(\\d+)");
 
     private PostController controller;
@@ -24,6 +19,8 @@ public class MainServlet extends HttpServlet {
         var context = new AnnotationConfigApplicationContext(AppConfig.class);
         controller = context.getBean(PostController.class);
     }
+
+
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) {
@@ -35,16 +32,16 @@ public class MainServlet extends HttpServlet {
                 controller.all(resp);
                 return;
             }
-            if (method.equals("GET") && path.matches(POSTS_ID_PATH)) {
+            if (method.equals("GET") && path.matches(MainServlet.POSTS_ID_PATH)) {
                 final var id = extractId(path);
                 controller.getById(id, resp);
                 return;
             }
-            if (method.equals("POST") && path.equals(POSTS_PATH)) {
+            if (method.equals("POST") && path.equals(MainServlet.POSTS_PATH)) {
                 controller.save(req.getReader(), resp);
                 return;
             }
-            if (method.equals("DELETE") && path.matches(POSTS_ID_PATH)) {
+            if (method.equals("DELETE") && path.matches(MainServlet.POSTS_ID_PATH)) {
                 final var id = extractId(path);
                 controller.removeById(id, resp);
                 return;
@@ -57,7 +54,7 @@ public class MainServlet extends HttpServlet {
     }
 
     private long extractId(String path) {
-        var matcher = ID_PATTERN.matcher(path);
+        var matcher = MainServlet.ID_PATTERN.matcher(path);
         if (matcher.find()) {
             return Long.parseLong(matcher.group(1));
         } else {
